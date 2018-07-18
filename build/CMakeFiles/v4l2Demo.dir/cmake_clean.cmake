a1fbded490da802c7f4c852b5bf2f5e7de93fdf7
anheidelonghu <1207186994@qq.com>
2018-07-18 03:18:45 +0000

    fix CMakeLists.txt

--- build/CMakeFiles/v4l2Demo.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/v4l2Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/v4l2Demo.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for v4l2Demo.
-# This may be replaced when dependencies are built.
